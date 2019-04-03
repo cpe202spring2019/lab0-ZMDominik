07e83a3fab3388f235b212f1c96773dca6418b4a
ZDominik <zuzia.dominik@gmail.com>
2019-04-03 10:24:12 -0700

    Update planets.py

--- planets.py
+++ planets.py
@@ -1,5 +1,7 @@
 def weight_on_planets():
-   # write your code here
+   weight = float(input('What do you weigh on Earth? '))
+   print('\nOn Mars you would weigh %.2f' %(weight*.38), ' pounds.\nOn Jupiter you would weigh %.2f'%(weight*2.34), 'pounds.')
+
    
    
    
